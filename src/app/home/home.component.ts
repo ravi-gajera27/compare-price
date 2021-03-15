cab47e4030288e20ddcc4cb03a91ed86c9406ebe
ravi-gajera27 <gajeraravi42@gmail.com>
2021-03-15 17:30:09 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +9,33 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dataService: DataService, private router: Router) { }
+  constructor(private dataService: DataService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe((param) => {
+      if (param.text) {
+        this.available = true
+        this.search_word = param.text
+        this.dataService.getPoducts(this.search_word).subscribe((res) => {
+          this.products = res.data;
+          if (this.products.length == 0) {
+            this.available = false
+          }
+        })
+      }
+    })
   }
 
   products: any[]
   search_word = ''
   available = true
   search() {
-    this.available = true
-    this.dataService.getPoducts(this.search_word).subscribe((res) => {
-      this.products = res.data;
-      if(this.products.length == 0){
-        this.available = false
-      }
-    })
+    this.router.navigate(['/search/' + this.search_word])
   }
 
-  
-  compareProduct(id, title){
+
+  compareProduct(id, title) {
     this.router.navigate(['/compare/' + title + '/' + id])
   }
 
