adc7c84f54ff17a8cce92d8b2ba612e8dbbd0393
ravi-gajera27 <gajeraravi42@gmail.com>
2021-05-17 16:31:02 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,42 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../services/data.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../services/data.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private dataService: DataService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-
     this.route.params.subscribe((param) => {
       if (param.text) {
-        this.available = true
-        this.search_word = param.text
+        this.available = true;
+        this.search_word = param.text;
         this.dataService.getPoducts(this.search_word).subscribe((res) => {
           this.products = res.data;
           if (this.products.length == 0) {
-            this.available = false
+            this.available = false;
           }
-        })
+        });
       }
-    })
+    });
   }
 
-  products: any[]
-  search_word = ''
-  available = true
+  products: any[];
+  search_word = "";
+  available = true;
   search() {
-    this.router.navigate(['/search/' + this.search_word])
+    this.router.navigate(["/search/" + this.search_word]);
   }
 
+  logout() {
+    localStorage.removeItem("auth_token");
+    this.router.navigate(["/login"]);
+  }
 
   compareProduct(id, title) {
-    this.router.navigate(['/compare/' + title + '/' + id])
+    this.router.navigate(["/compare/" + title + "/" + id]);
   }
-
 }
